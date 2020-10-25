d21feb597edfe8e856eca1a6bd09dfba85607462
albert-Yjz <1517488035@qq.com>
2020-10-25 21:51:14 +0800

    second

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <!--所有的item都动态展示文字-->
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+    /*计算组件是否活跃*/
+    computed: {
+      isActive() {
+        // /home -> item1(/home) = true
+        // /home -> item1(/category) = false
+        // 通过看当前路劲是否有我这个路径的名称来判断是否点击
+        /*$route.path是拿到当前活跃路径，this.path是当前路径*/
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "tabControl"
+  }
+</script>
+
+<style scoped>
+
+</style>
